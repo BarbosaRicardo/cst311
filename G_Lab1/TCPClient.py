ec23e12b898d4ac3cb376ab24facc07b28419aad
Ricardo Barbosa <ric3639@gmail.com>
2020-05-10 15:21:28 -0700

    TCPClient.py MVO

--- G_Lab1/TCPClient.py
+++ G_Lab1/TCPClient.py
@@ -7,13 +7,30 @@ The client receives the modified data and displays the line on its screen
 
 '''
 
+# socket module
 from socket import *
+
+# initialize variables
 serverName = '127.0.0.1'
 serverPort = 12000
+
+# create client socket; IPv4/TCP socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# initiate TCP connection between client and server
 clientSocket.connect((serverName, serverPort))
+
+# receive user input until carriage return
 sentence = input('Input lowercase sentence: ')
+
+# user input sent through client's socket, into TCP connection
 clientSocket.send(sentence.encode())
+
+# user input on server side is combined until received in full
 modifiedSentence = clientSocket.recv(1024)
+
+# user input prints 
 print('From Server: ', modifiedSentence.decode())
+
+# socket and TCP connection close between client and server
 clientSocket.close()
