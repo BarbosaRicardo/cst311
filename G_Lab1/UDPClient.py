afb4edfd680ce8c905b1c20ede0975817c7705ec
Ricardo Barbosa <ric3639@gmail.com>
2020-05-10 20:49:35 -0700

    UDPClient MVP

--- G_Lab1/UDPClient.py
+++ G_Lab1/UDPClient.py
@@ -7,13 +7,23 @@ The client receives the modified data and displays the line on its screen
 
 '''
 
-from socket import *       # allows the creation of sockets 
+# socket module
+from socket import *      
 
+# initialize variables
 serverName = '127.0.0.1'    
 serverPort = 12000
+
+# create client socket; IPv4/UDP
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+# create message  to send to client 
 message = input('Input lowercase sentence: ')
+
+# encode message from string type to byte type, send to its destination address
 clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+# receives message and stores in variable modifiedMessage; buffer size of 2048
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
 clientSocket.close()
