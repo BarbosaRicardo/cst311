74804b8a51c0d6a7bd3fadc44e2c8bf35d1fcca8
Ricardo Barbosa <ric3639@gmail.com>
2020-05-10 20:11:57 -0700

    TCPServer mvp

--- G_Lab1/TCPServer.py
+++ G_Lab1/TCPServer.py
@@ -7,14 +7,25 @@ The server sends the modified data to the client
 
 '''
 
+# socket module
 from socket import *
+
+# initialize variables
 serverPort = 12000
+
+# server creates a TCP socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# associate server port with TCP socket
 serverSocket.bind(('', serverPort))
+
+# wait for client 
 serverSocket.listen(1)
 print('The server is ready to receive')
 
+# while client is waited on
 while True:
+   # accepts client and creates new socket (connectionSocket) in the server
    connectionSocket, addr = serverSocket.accept()
    sentence = connectionSocket.recv(1024).decode()
    capitalizedSentence = sentence.upper()
